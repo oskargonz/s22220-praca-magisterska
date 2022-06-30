c5f5d5ac773711c47aaa15f39acd8a51c4901a67
oskargonz <oskargasior@gmail.com>
2022-06-30 20:30:19 +0200

    finished-app-conf

--- s22220-mgr-Oskar-Gasior/src/main/scala/spark/MainSpark3.scala
+++ s22220-mgr-Oskar-Gasior/src/main/scala/spark/MainSpark3.scala
@@ -2,7 +2,7 @@ package spark
 
 import org.apache.log4j.Logger
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
 import java.util.Properties
 import scala.io.Source
@@ -13,14 +13,35 @@ object MainSpark3 extends Serializable {
 
   def main(args: Array[String]): Unit = {
 
+    if (args.length == 0) {
+      logger.error("Usage: input data not found.")
+      System.exit(1)
+    }
+
     logger.info("Starting s22220-mgr application.")
 
     val spark: SparkSession = SparkSession.builder()
       .config(getSparkAppConf)
       .getOrCreate()
 
+    val covidDF: DataFrame = loadDF(spark, args(0))
+    val partitionedCovidDF: Dataset[Row] = covidDF.repartition(4)
+    val countTopCountriesDF: DataFrame = SampleTransformations.topCountries(partitionedCovidDF)
+
+    // countTopCountriesDF.show(false) // Uncomment this line to see result of topCountries method.
+    logger.info(countTopCountriesDF.collect().mkString("->")) // Action
+
     logger.info("Finished s22220-mgr application.")
+    //scala.io.StdIn.readLine() // Uncomment this line to use Spark Web UI
     spark.stop()
+  }
+
+  def loadDF(spark: SparkSession, dataFile: String): DataFrame = {
+    spark.read // Action
+      .option("header", "true")
+      .option("inferSchema", "true") // Action
+      .csv(dataFile)
+
   }
 
   def getSparkAppConf: SparkConf = {
---
+++ s22220-mgr-Oskar-Gasior/src/test/scala/MainSpark3Test.scala
@@ -0,0 +1,44 @@
+import org.apache.spark.sql.SparkSession
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.funsuite.AnyFunSuite
+import spark.MainSpark3.loadDF
+import spark.SampleTransformations
+
+import scala.collection.mutable
+
+
+
+class MainSpark3Test extends AnyFunSuite with BeforeAndAfterAll{
+
+  @transient var spark: SparkSession = _
+
+  override def beforeAll(): Unit = {
+    spark = SparkSession.builder()
+      .appName("s22220-mgr")
+      .master("local[3]")
+      .getOrCreate()
+  }
+
+  override def afterAll(): Unit = {
+    spark.stop()
+  }
+
+  test("Test 1 - loadDF") {
+    val covidDF = loadDF(spark, "data/covid19_data_NZ.csv")
+    val countRows = covidDF.count()
+    assert(countRows == 111438)
+  }
+
+  test("Test 2 - topCountries") {
+    val covidDF = loadDF(spark, "data/covid19_data_NZ.csv")
+    val countDF = SampleTransformations.topCountries(covidDF)
+    val countryMap = new mutable.HashMap[String, Long]
+    countDF.collect().foreach(r => countryMap.put(r.getString(0), r.getLong(1)))
+    assert(countryMap("East Asia (excluding China)") == 31)
+    assert(countryMap("All") == 9528)
+    assert(countryMap("China") == 92)
+    assert(countryMap("Total (excluding China)") == 1537)
+    assert(countryMap("United States") == 3)
+  }
+
+}
